dc0d195eae548565aa5ed09211a7251bdf8f5588
Yashwantcine <cinemicabot2024@gmail.com>
2024-08-13 13:59:36 +0530

    Update __init__.py

--- biisal/server/__init__.py
+++ biisal/server/__init__.py
@@ -1,10 +1,27 @@
 # © agrprojects
 
+import ssl
 from aiohttp import web
 from .stream_routes import routes
 
-
 async def web_server():
+    # Initialize the web application
     web_app = web.Application(client_max_size=30000000)
     web_app.add_routes(routes)
-    return web_app
+
+    # Set up SSL context
+    ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+    ssl_context.load_cert_chain(
+        certfile='biisal/server/ssl_cert/certificate.crt', 
+        keyfile='biisal/server/ssl_cert/private.key'
+    )
+
+    # Load CA Bundle
+    ssl_context.load_verify_locations(cafile='biisal/server/ssl_cert/ca_bundle.crt')
+
+    # Start the web server with SSL
+    return web_app, ssl_context
+
+if __name__ == "__main__":
+    app, ssl_context = web_server()
+    web.run_app(app, port=9010, ssl_context=ssl_context)
